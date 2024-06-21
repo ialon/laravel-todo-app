a039c3e8fd74a11be2a7453dd04bedccc4e3f023
Josemaria Bolanos <josemaria.bolanos@kineo.com>
2024-06-21 00:39:02 -0600

    Layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,16 @@
-<div>
-    List of tasks
-</div>
+@extends('layouts.app')
 
-<div>
-    @forelse ($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-            <p>{{ $task->description }}</p>
-        </div>
-    @empty
-        <p>No tasks available</p>
-    @endforelse
-</div>
+@section('title', 'List of tasks')
+
+@section('content')
+    <div>
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+                <p>{{ $task->description }}</p>
+            </div>
+        @empty
+            <p>No tasks available</p>
+        @endforelse
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Laravel Todo App</title>
+</head>
+<body>
+    <h1>
+        @yield('title', 'My Laravel Todo App')
+    </h1>
+    <div>
+        @yield ('content')
+    </div>
+</body>
+</html>
